5729a84c4463af85dbe92550c51fe397f10bcf0b
Igor Sereda <sereda.igor.s@gmail.com>
2024-04-17 23:08:13 +0300

    Add test tokens to config

--- src/services/cfg/tokens.js
+++ src/services/cfg/tokens.js
@@ -59,6 +59,67 @@ export const tokenPairs = [
 			icon: "fxhash",
 		},
 	},
+	{
+		tezos: {
+			type: "fa1.2",
+			name: "tzBTC",
+			ticker: "tzBTC",
+			address: "KT1CTpSVM5iwL5GGCA7Dixzbp8qwpnthNirm",
+			decimals: 8,
+			icon: "tzbtc",
+			ticketerContractAddress: "KT1SoFYsd6jqf7SPNUbriUKGbmcNjnMzZQFY",
+			ticketHelperContractAddress: "KT19HX9LJvsEcjBvadB2YzELUJ3zNuXMVKyu",
+		},
+		etherlink: {
+			type: "erc20",
+			name: "tzBTC",
+			ticker: "tzBTC",
+			address: "0x80d33a72b6d13b44da36c2a93a8cb6e1d451eb2a",
+			decimals: 8,
+			icon: "tzbtc",
+		},
+	},
+	{
+		tezos: {
+			type: "fa1.2",
+			name: "Sirius",
+			ticker: "SIRS",
+			address: "KT1FLMJPxJeyYzUSB9Yt3UptD2oXkwG84rUv",
+			decimals: 0,
+			icon: "sirs",
+			ticketerContractAddress: "KT1TLCPyP1xwLmwXqTh5QtyL7jhXKEaABaLV",
+			ticketHelperContractAddress: "KT1TFfzJzRNjAJcjDAZBrHfAbyy315ZjVrVK",
+		},
+		etherlink: {
+			type: "erc20",
+			name: "Sirius",
+			ticker: "SIRS",
+			address: "0x1144de4736cc0e83ab7c8e51872eac2015629e07",
+			decimals: 0,
+			icon: "sirs",
+		},
+	},
+	{
+		tezos: {
+			type: "fa2",
+			name: "Tether USD",
+			ticker: "USDt",
+			address: "KT1PpsQEFEg6ZbLwqv7SWW9UDQA7dmjVeF1d",
+			tokenId: "0",
+			decimals: 6,
+			icon: "usdt",
+			ticketerContractAddress: "KT1MNrN1XUxZMPSy2NZAGUhk1dnY8MFtA5eu",
+			ticketHelperContractAddress: "KT1KRSC4UWEoPoZtrwFUZ8nDsL4cGZbJY4e3",
+		},
+		etherlink: {
+			type: "erc20",
+			name: "Tether USD",
+			ticker: "USDt",
+			address: "0x5b69d8c28fb0f766a32fd9ac8b0f5fb5f2104f88",
+			decimals: 6,
+			icon: "usdt",
+		},
+	},
 ]
 
 export const getTokenKey = (token) => {
